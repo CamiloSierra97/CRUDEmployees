2628c9b96d8dabacdcf62e2441b60b84fed43622
Camilo Sierra <juanca201015@hotmail.com>
2024-01-12 05:27:45 -0500

    Create Read Update(need to be fixed) employee

--- CRUDEmployees/Controllers/EmployeesController.cs
+++ CRUDEmployees/Controllers/EmployeesController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using CRUDEmployees.Models;
 using CRUDEmployees.Models.WS;
+using Microsoft.Ajax.Utilities;
 
 namespace CRUDEmployees.Controllers
 {
@@ -90,6 +91,85 @@ namespace CRUDEmployees.Controllers
             Reply oR = new Reply();
             oR.result = 0;
 
+            //Validations
+            if (!Validate(model))
+            {
+                oR.Message = error;
+                return oR;
+            }
+
+            try
+            {
+                using (DBEMPLOYEESEntities dBEMPLOYEES = new DBEMPLOYEESEntities())
+                {
+                    Employees oEmployee = dBEMPLOYEES.Employees.Find(model.EmployeeId);
+                    oEmployee.FirstName = model.FirstName;
+                    oEmployee.Surname = model.Surname;
+                    oEmployee.Email = model.Email;
+                    oEmployee.DocumentType = model.DocumentType;
+                    oEmployee.DocumentType = model.DocumentType;
+                    oEmployee.DateOfHire = model.DateOfHire;
+                    oEmployee.Phone = model.Phone;
+
+                    //Validate Area and Subarea
+                    var subArea = dBEMPLOYEES.SubArea.Find(model.SubAreaId);
+                    var country = dBEMPLOYEES.Country.Find(model.CountryId);
+
+                    if (subArea != null && country != null)
+                    {
+                        oEmployee.SubAreaId = model.SubAreaId;
+                        oEmployee.CountryId = model.CountryId;
+
+                        dBEMPLOYEES.Entry(oEmployee).State = System.Data.Entity.EntityState.Modified;
+                        dBEMPLOYEES.SaveChanges();
+
+                        oR.result = 1;
+                        oR.Message = "Employee added succesfully";
+
+                        List<EmployeeViewModel> lst = (from e in dBEMPLOYEES.Employees
+                                                       select new EmployeeViewModel
+                                                       {
+                                                           FirstName = e.FirstName,
+                                                           Surname = e.Surname,
+                                                           Email = e.Email,
+                                                           DocumentType = e.DocumentType,
+                                                           DocumentNumber = e.DocumentNumber,
+                                                           DateOfHire = e.DateOfHire,
+                                                           Phone = e.Phone,
+                                                       }).ToList();
+                        oR.data = lst;
+                        oR.result = 1;
+                    }
+                    else
+                    {
+                        oR.Message = "Invalid SubAreaId or CountryId provided.";
+                    }
+
+                }
+                
+            }
+            catch
+            {
+                oR.Message = "Server error, try again later";
+            }
+
+            return oR;
+        }
+
+        //Edit Employee
+        [HttpPost]
+        public Reply EditEmployee(EmployeeViewModel model)
+        {
+            Reply oR = new Reply();
+            oR.result = 0;
+
+            //Validations
+            if (!Validate(model))
+            {
+                oR.Message = error;
+                return oR;
+            }
+
             try
             {
                 using (DBEMPLOYEESEntities dBEMPLOYEES = new DBEMPLOYEESEntities())
@@ -130,6 +210,7 @@ namespace CRUDEmployees.Controllers
                                                            Phone = e.Phone,
                                                        }).ToList();
                         oR.data = lst;
+                        oR.result = 1;
                     }
                     else
                     {
@@ -137,7 +218,7 @@ namespace CRUDEmployees.Controllers
                     }
 
                 }
-                
+
             }
             catch
             {
@@ -147,6 +228,22 @@ namespace CRUDEmployees.Controllers
             return oR;
         }
 
-        //#region
+        #region HELPERS
+
+        public string error = "";
+        private bool Validate(EmployeeViewModel model)
+        {
+            if (model.FirstName.IsNullOrWhiteSpace() ||
+                model.Surname.IsNullOrWhiteSpace() ||
+                model.Email.IsNullOrWhiteSpace() ||
+                model.DocumentType.IsNullOrWhiteSpace() ||
+                model.DateOfHire.ToString().IsNullOrWhiteSpace())
+            {
+                error = "Missing Data";
+                return false;
+            }
+            return true;
+        }
+        #endregion
     }
 }
